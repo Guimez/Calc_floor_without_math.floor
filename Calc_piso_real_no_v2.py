4fa266cd62971e50b96d40244baaae4517e120be
Guilherme Módolo <83830962+Guimez@users.noreply.github.com>
2024-02-29 11:55:55 -0300

    Update Calc_piso_real_no_v2.py

--- Calc_piso_real_no_v2.py
+++ Calc_piso_real_no_v2.py
@@ -3,17 +3,12 @@ def main():
     Number = float(input("\n=====\nDo you want to find the floor of which number? "))
     print(f"=====\nThe floor of {Number} is:", piso(Number),f"\n=====\n{Number} times its floor is:", round(Number * piso(Number), 6),"\n")
 
-#Defines a function to calculate the floor. 
+#Defines a function to calculate the floor.
 def piso(n):
-    if round(n) <= n:
-        return round(n)
-    else:
         if n < 0:
             return int(n) - 1
-        if n > 0:
+        
+        elif n >= 0:
             return int(n)
 
 main()
-
-
-
